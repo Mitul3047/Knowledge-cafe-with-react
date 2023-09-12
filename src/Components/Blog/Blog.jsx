1315151cf9aca2a83adbf06849bdc39442255982
Shehab Ahamed Chowdhury Mitul <121333557+Mitul3047@users.noreply.github.com>
2023-09-12 18:05:08 +0600

    Envioronment and Components create

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -16,9 +16,3 @@ Blog.propTypes ={
 export default Blog;
 
 
-git init
-git add .
-git commit -m "Envioronment and Components create"
-git branch -M main
-git remote add origin https://github.com/Mitul3047/Knowledge-cafe-with-react.git
-git push -u origin main
